6998c3d349e49aa3f5c10aeca47891eda87929ae
olemarz <om@adspirin.com>
2025-10-19 20:06:21 +0300

    Fix duplicate promptGeoTargeting definitions

--- src/bot/html.js
+++ src/bot/html.js
@@ -1,12 +1,11 @@
-const WL = /<(?:\/?(?:b|strong|i|em|u|s|strike|del|a|code|pre|br)\b[^>]*)>/gi;
+const WL = /<(\/?(?:b|i|u|a|code|pre|br)(?:\s+[^>]*)?)>/gi;
 
 export function sanitizeTelegramHtml(input) {
   if (!input) return '';
   let s = String(input);
   s = s.replace(/\r?\n/g, '<br>');
   s = s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-  s = s.replace(/&lt;(\/?(?:b|strong|i|em|u|s|strike|del|a|code|pre|br)\b[^&]*)&gt;/gi, '<$1>');
-  s = s.replace(/(?:<br>\s*){3,}/g, '<br><br>');
+  s = s.replace(/&lt;(\/?(?:b|i|u|a|code|pre|br)(?:\s+[^&>]*)?)&gt;/gi, '<$1>');
   return s;
 }
 
