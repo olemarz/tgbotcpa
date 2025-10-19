ed69391b6e44d9573b1209215bc50ea8fcf01d56
deploy <deploy@adspirin.ru>
2025-10-19 20:57:32 +0000

    html: normalize <br/> to <br> for Telegram HTML parser

--- src/bot/html.js
+++ src/bot/html.js
@@ -1,27 +1,20 @@
 const WL = /<(\/?(?:b|i|u|a|code|pre|br)(?:\s+[^>]*)?\/?)/gi;
 
 export function sanitizeTelegramHtml(input) {
-  if (!input) return '';
+  if (input == null) return '';
   let s = String(input);
-  s = s.replace(/\r?\n/g, '<br/>');
+
+  // \n -> <br>
+  s = s.replace(/\r?\n/g, '<br>');
+
+  // Экраним всё
   s = s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-  s = s.replace(/&lt;br\s*\/??&gt;/gi, '<br/>');
-  s = s.replace(
-    /&lt;(\/?)(b|i|u|a|code|pre)(\s+[^&>]*)?&gt;/gi,
-    (_, slash = '', tag = '', attrs = '') => `<${slash}${tag}${attrs || ''}>`,
-  );
+
+  // Разрешаем подмножество тегов
+  s = s.replace(/&lt;(\/?(?:b|strong|i|em|u|s|a|code|pre|br)\b[^&]*)&gt;/gi, '<$1>');
+
+  // 🔧 НОРМАЛИЗУЕМ <br/> и <br /> в <br> (Telegram не любит самозакрывающийся br)
+  s = s.replace(/<br\s*\/>/gi, '<br>');
+
   return s;
-}
-
-export async function replyHtml(ctx, html, extra = {}) {
-  let value = html;
-  if (typeof value !== 'string') {
-    if (value === null || value === undefined) value = '';
-    else if (Array.isArray(value)) value = value.join('\n');
-    else if (typeof value.toString === 'function') value = value.toString();
-    else value = String(value);
-  }
-
-  const safe = sanitizeTelegramHtml(value);
-  return ctx.reply(safe, { parse_mode: 'HTML', disable_web_page_preview: true, ...extra });
 }
