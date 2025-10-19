84ce9d2d85eb78aee30fa7101dd25d2d09ae5caa
olemarz <om@adspirin.com>
2025-10-19 23:24:27 +0300

    fix(wizard): stabilize /ads scene, fix HTML entities, remove br

--- src/bot/html.js
+++ src/bot/html.js
@@ -1,15 +1,33 @@
-const WL = /<(\/?(?:b|i|u|a|code|pre|br)(?:\s+[^>]*)?)>/gi;
+const WL = /&lt;(\/?(?:b|i|u|a|code|pre)(?:\s+[^&>]*)?)&gt;/gi;
 
 export function sanitizeTelegramHtml(input) {
-  if (!input) return '';
-  let s = String(input);
-  s = s.replace(/\r?\n/g, '<br>');
+  if (input === null || input === undefined) return '';
+
+  let s;
+  if (typeof input === 'string') {
+    s = input;
+  } else if (typeof input.toString === 'function') {
+    s = input.toString();
+  } else {
+    s = String(input);
+  }
+
+  s = s.replace(/\r?\n/g, '\n');
   s = s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-  s = s.replace(/&lt;(\/?(?:b|i|u|a|code|pre|br)(?:\s+[^&>]*)?)&gt;/gi, '<$1>');
+  s = s.replace(WL, '<$1>');
+  s = s.replace(/<\/?br\s*\/?>(?:\n)?/gi, '\n');
   return s;
 }
 
 export async function replyHtml(ctx, html, extra = {}) {
-  const safe = sanitizeTelegramHtml(String(html));
+  let value = html;
+  if (typeof value !== 'string') {
+    if (value === null || value === undefined) value = '';
+    else if (Array.isArray(value)) value = value.join('\n');
+    else if (typeof value.toString === 'function') value = value.toString();
+    else value = String(value);
+  }
+
+  const safe = sanitizeTelegramHtml(value);
   return ctx.reply(safe, { parse_mode: 'HTML', disable_web_page_preview: true, ...extra });
 }
